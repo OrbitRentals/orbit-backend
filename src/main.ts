ec559794d19e823e1737a3e392e9238b9f63b325
OrbitRentals <huffmechanics@gmail.com>
2026-02-05 18:06:40 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,5 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  console.log(`🚀 Orbit API running on port ${port}`);
 }
-
 bootstrap();
