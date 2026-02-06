7cd3643b5161b833433999e86ac3d6aff5ff1426
OrbitRentals <huffmechanics@gmail.com>
2026-02-05 20:48:47 -0600

    Fix CORS preflight once and for all

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*',
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Content-Type, Authorization',
+    allowedHeaders: '*',
+    credentials: true,
   });
 
   await app.listen(process.env.PORT || 3000);
 }
-
 bootstrap();
