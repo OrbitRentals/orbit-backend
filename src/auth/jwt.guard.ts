cc4f3e38fe8585471d43e0ee8b9bb9e72edd35ce
OrbitRentals <huffmechanics@gmail.com>
2026-02-05 23:14:59 -0600

    Nest application successfully started

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,19 +1,5 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
+import { Injectable } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtGuard implements CanActivate {
-  constructor(private jwt: JwtService) {}
-
-  canActivate(context: ExecutionContext): boolean {
-    const req = context.switchToHttp().getRequest();
-    const auth = req.headers.authorization;
-    if (!auth) return false;
-    try {
-      req.user = this.jwt.verify(auth.replace('Bearer ', ''));
-      return true;
-    } catch {
-      return false;
-    }
-  }
-}
+export class JwtAuthGuard extends AuthGuard('jwt') {}
